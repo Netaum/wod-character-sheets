82abb9d9ab6488626351cdbcc0a9aa6bff86624e
Antonio Felipe Cechi Neto <pronetao@gmail.com>
2018-11-08 14:56:37 -0200

    Better response on fill

--- js/sheet.js
+++ js/sheet.js
@@ -9,67 +9,37 @@ $(function(){
     function swap(text, index, charIn, charOut){
         var len = text.length;
         var newText = "";
-        for(i = 0; i < len; i++){
-            var curText = text.substring(i, i+1);
+        for(i = 1; i <= len; i++){
+            var curText = text.substring(i-1, i);
             if(curText === " "){
-                newText += " ";
+                newText += `<span vl="${i}"> </span>`;
                 continue;
             }
             if(i < index){
-                newText += charOut;
+                newText += `<span vl="${i}">${charOut}</span>`;
                 continue;
             }
             if (i === index && curText === charIn){
-                newText += charOut;
+                newText += `<span vl="${i}">${charOut}</span>`;
                 continue;
             }
-            newText += charIn;
+            newText += `<span vl="${i}">${charIn}</span>`;
         }
 
         return newText;
     }
 
-    $(".wod-att").click(function(){
-        var getSelection = window.getSelection();
-        var selection = getSelection.focusOffset;
+    $(".wod-att").click(function(eventData){
+        var selection = Number(eventData.target.attributes["vl"].value);
         var text = $(this).text();
         var newText = swap(text, selection, "d", "e");
-        $(this).text(newText);
+        $(this).html(newText);
     });
 
-    $(".wod-square").click(function(){
-        var getSelection = window.getSelection();
-        var selection = getSelection.focusOffset;
+    $(".wod-square").click(function(eventData){
+        var selection = Number(eventData.target.attributes["vl"].value);
         var text = $(this).text();
         var newText = swap(text, selection, "f", "a");
-        $(this).text(newText);
-    });
-
-    $(".wod-att1").click(function(){
-        var getSelection = window.getSelection();
-        var selection = getSelection.focusOffset;
-        var text = $(this).text();
-        var newText = "";
-
-        for(i = 0; i < 5; i++){
-            
-            if(i < selection){
-                newText += "e";
-            }
-            else if(i === selection){
-                var f = text.substring(i,i+1);
-                if(f === 'd'){
-                    newText += "e";
-                }
-                else{
-                    newText += "d"
-                }
-            }
-            else{
-                newText += "d"
-            }
-        }
-
-        $(this).text(newText);
+        $(this).html(newText);
     });
 });
