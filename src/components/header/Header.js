7f56417bb677a7a06685aeb1593b2343f9fb4a11
Antonio Felipe Cechi Neto <antoniofc@ciandt.com>
2020-01-21 08:21:09 -0300

    Translate

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,45 +2,19 @@ import Input from '../input/Input'
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-
-var predatorTypes = [
-    "Alleycat",
-    "Bagger",
-    "Blood Leech",
-    "Cleaver",
-    "Consensualist",
-    "Farmer",
-    "Osiris",
-    "Sandman",
-    "Scene Queen",
-    "Siren"
-];
-
-var clanNames = [
-    "Brujah",
-    "Gangrel",
-    "Malkavian",
-    "Nosferatu",
-    "Toreador",
-    "Tremere",
-    "Ventrue",
-    "Caitiff",
-    "Thin-Blooded"
-];
-
 function Header() {
     const { t } = useTranslation();
     return(
         <div>
-            <Input name={t('common:header.name')} column="c1" line="l1" type="input" />
-            <Input name="Concept" column="c1" line="l2" type="input" />
-            <Input name="Predator" column="c1" line="l3" type="dropdown" options={predatorTypes} />
-            <Input name="Chronicle" column="c2" line="l1" type="input" />
-            <Input name="Ambition" column="c2" line="l2" type="input" />
-            <Input name="Clan" column="c2" line="l3" type="dropdown" options={clanNames} />
-            <Input name="Sire" column="c3" line="l1" type="input" />
-            <Input name="Desire" column="c3" line="l2" type="input" />
-            <Input name="Generation" column="c3" line="l3" type="input" />
+            <Input name={t('header:name')} column="c1" line="l1" type="input" />
+            <Input name={t('header:concept')} column="c1" line="l2" type="input" />
+            <Input name={t('header:predator')} column="c1" line="l3" type="dropdown" options={t('header:predatorTypes', { returnObjects: true })} />
+            <Input name={t('header:chronicle')} column="c2" line="l1" type="input" />
+            <Input name={t('header:ambition')} column="c2" line="l2" type="input" />
+            <Input name={t('header:clan')} column="c2" line="l3" type="dropdown" options={t('header:clanNames', { returnObjects: true })} />
+            <Input name={t('header:sire')} column="c3" line="l1" type="input" />
+            <Input name={t('header:desire')} column="c3" line="l2" type="input" />
+            <Input name={t('header:generation')} column="c3" line="l3" type="input" />
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -9,19 +9,23 @@ import { initReactI18next } from "react-i18next";
 
 import common_en from  "./translations/en/common.json";
 import common_pt from  "./translations/pt/common.json";
+import header_en from "./translations/en/header.json";
+import header_pt from "./translations/pt/header.json";
 
 i18n
   .use(initReactI18next) 
   .init({
     resources: {
       en: {
-        common: common_en
+        common: common_en,
+        header: header_en
       },
       pt: {
-        common: common_pt
+        common: common_pt,
+        header: header_pt
       }
     },
-    lng: "en",
+    lng: "pt",
     fallbackLng: "en",
 
     interpolation: {
